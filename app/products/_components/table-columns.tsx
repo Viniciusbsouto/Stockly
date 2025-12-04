7bb9d6dfd65679cc9e6b50763b7dbf62764f6b8e
Vinicius <viniciusbsouto@gmail.com>
2025-12-04 14:37:51 -0300

    feat: add initial product dropdown menu

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -2,7 +2,22 @@
 
 import { Badge } from "@/app/_components/ui/badge";
 import { Product } from "@prisma/client";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+} from "@/app/_components/ui/dropdown-menu";
 import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/app/_components/ui/button";
+import {
+  ClipboardCopyIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+  Trash2Icon,
+} from "lucide-react";
 
 // Status será derivado de `stock` (stock > 0 => "Em estoque")
 
@@ -20,13 +35,48 @@ export const productTableColumns: ColumnDef<Product>[] = [
     header: "Estoque",
   },
   {
-    id: "status",
+    accessorKey: "status",
     header: "Status",
     cell: (row) => {
       const product = row.row.original;
       const inStock = product.stock > 0;
       const label = inStock ? "Em estoque" : "Fora de estoque";
       return <Badge variant={inStock ? "default" : "outline"}>{label}</Badge>;
+    },
+  },
+  {
+    accessorKey: "actions",
+    header: "Ações",
+    cell: (row) => {
+      const product = row.row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              className="gap-1.5"
+              onClick={() => navigator.clipboard.writeText(product.id)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <EditIcon size={16} className="gap-1.5" />
+              Editar
+            </DropdownMenuItem>
+            <DropdownMenuItem className="gap-1.5">
+              <Trash2Icon size={16} />
+              Deletar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
