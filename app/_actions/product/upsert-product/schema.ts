e0afb3b392c724c7438c78d76ca459007f13a63a
Vinicius <viniciusbsouto@gmail.com>
2025-12-04 16:02:33 -0300

    feat: ensure product can be updated

---
+++ app/_actions/product/upsert-product/schema.ts
@@ -0,0 +1,16 @@
+import z from "zod";
+
+export const upsertProductSchema = z.object({
+  id: z.string().uuid().optional(),
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: "O nome do produto é obrigatório" }),
+  price: z.number().min(0.01, { message: "O preço do produto é obrigatório" }),
+  stock: z.coerce
+    .number()
+    .positive({ message: "A quantidade do estoque deve ser maior que zero" })
+    .int(),
+});
+
+export type UpsertProductSchema = z.infer<typeof upsertProductSchema>;
--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -23,6 +23,9 @@ import {
   AlertDialogTrigger,
 } from "@/app/_components/ui/alert-dialog";
 import DeleteProductDialogContent from "./delete-dialog-content";
+import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
+import UpsertProductDialogContent from "./upsert-dialog-content";
+import { useState } from "react";
 
 // Status será derivado de `stock` (stock > 0 => "Em estoque")
 
@@ -53,39 +56,53 @@ export const productTableColumns: ColumnDef<Product>[] = [
     accessorKey: "actions",
     header: "Ações",
     cell: (row) => {
+      const [editDialogIsOpen, setEditDialogIsOpen] = useState(false);
       const product = row.row.original;
       return (
-        <AlertDialog>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost">
-                <MoreHorizontalIcon size={16} />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>Ações</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="gap-1.5"
-                onClick={() => navigator.clipboard.writeText(product.id)}
-              >
-                <ClipboardCopyIcon size={16} />
-                Copiar ID
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <EditIcon size={16} className="gap-1.5" />
-                Editar
-              </DropdownMenuItem>
-              <AlertDialogTrigger asChild>
-                <DropdownMenuItem className="gap-1.5">
-                  <Trash2Icon size={16} />
-                  Deletar
+        <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
+          <AlertDialog>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost">
+                  <MoreHorizontalIcon size={16} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Ações</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="gap-1.5"
+                  onClick={() => navigator.clipboard.writeText(product.id)}
+                >
+                  <ClipboardCopyIcon size={16} />
+                  Copiar ID
                 </DropdownMenuItem>
-              </AlertDialogTrigger>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <DeleteProductDialogContent productId={product.id} />
-        </AlertDialog>
+                <DialogTrigger asChild>
+                  <DropdownMenuItem>
+                    <EditIcon size={16} className="gap-1.5" />
+                    Editar
+                  </DropdownMenuItem>
+                </DialogTrigger>
+                <AlertDialogTrigger asChild>
+                  <DropdownMenuItem className="gap-1.5">
+                    <Trash2Icon size={16} />
+                    Deletar
+                  </DropdownMenuItem>
+                </AlertDialogTrigger>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <DeleteProductDialogContent productId={product.id} />
+          </AlertDialog>
+          <UpsertProductDialogContent
+            defaultValues={{
+              id: product.id,
+              name: product.name,
+              price: Number(product.price),
+              stock: product.stock,
+            }}
+            onSuccess={() => setEditDialogIsOpen(false)}
+          />
+        </Dialog>
       );
     },
   },
