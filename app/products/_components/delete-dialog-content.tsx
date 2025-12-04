45d277013531e38970aa3cfa11750129c629a543
Vinicius <viniciusbsouto@gmail.com>
2025-12-04 15:02:08 -0300

    feat: ensure products can be deleted

---
+++ app/products/_components/delete-dialog-content.tsx
@@ -0,0 +1,50 @@
+import { deleteProduct } from "@/app/_actions/product/create-product/delete-product";
+import {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/app/_components/ui/alert-dialog";
+import { toast } from "sonner";
+
+interface DeleteProductDialogContentProps {
+  productId: string;
+}
+
+const DeleteProductDialogContent = ({
+  productId,
+}: DeleteProductDialogContentProps) => {
+  const handleContinueClick = async () => {
+    try {
+      await deleteProduct({ id: productId });
+      toast.success("Produto deletado com sucesso!");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao deletar o produto. Tente novamente.");
+    }
+  };
+  return (
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>
+          Você realmente deseja deletar este produto?
+        </AlertDialogTitle>
+        <AlertDialogDescription>
+          Esta ação não pode ser desfeita. Isso irá deletar permanentemente o
+          produto e remover seus dados. Deseja continuar?
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancelar</AlertDialogCancel>
+        <AlertDialogAction onClick={handleContinueClick}>
+          Continuar
+        </AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  );
+};
+
+export default DeleteProductDialogContent;
--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -18,6 +18,11 @@ import {
   MoreHorizontalIcon,
   Trash2Icon,
 } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
+import DeleteProductDialogContent from "./delete-dialog-content";
 
 // Status será derivado de `stock` (stock > 0 => "Em estoque")
 
@@ -50,32 +55,37 @@ export const productTableColumns: ColumnDef<Product>[] = [
     cell: (row) => {
       const product = row.row.original;
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost">
-              <MoreHorizontalIcon size={16} />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>Ações</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="gap-1.5"
-              onClick={() => navigator.clipboard.writeText(product.id)}
-            >
-              <ClipboardCopyIcon size={16} />
-              Copiar ID
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <EditIcon size={16} className="gap-1.5" />
-              Editar
-            </DropdownMenuItem>
-            <DropdownMenuItem className="gap-1.5">
-              <Trash2Icon size={16} />
-              Deletar
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost">
+                <MoreHorizontalIcon size={16} />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Ações</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="gap-1.5"
+                onClick={() => navigator.clipboard.writeText(product.id)}
+              >
+                <ClipboardCopyIcon size={16} />
+                Copiar ID
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <EditIcon size={16} className="gap-1.5" />
+                Editar
+              </DropdownMenuItem>
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem className="gap-1.5">
+                  <Trash2Icon size={16} />
+                  Deletar
+                </DropdownMenuItem>
+              </AlertDialogTrigger>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DeleteProductDialogContent productId={product.id} />
+        </AlertDialog>
       );
     },
   },
