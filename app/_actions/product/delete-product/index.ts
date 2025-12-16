a2d80b480ab61ab7e95dce7f4bc6389e8c478f7b
Vinicius <viniciusbsouto@gmail.com>
2025-12-16 16:02:44 -0300

    feat: ensure delete product action uses next-safe-action

--- app/_actions/product/delete-product/index.ts
+++ app/_actions/product/delete-product/index.ts
@@ -1,15 +1,18 @@
 "use server";
 
 import { db } from "@/app/_lib/prisma";
-import { deleteProductSchema, DeleteProductSchema } from "./schema";
+import { deleteProductSchema } from "./schema";
 import { revalidatePath } from "next/cache";
 
-export const deleteProduct = async ({ id }: DeleteProductSchema) => {
-  deleteProductSchema.parse({ id });
-  await db.product.delete({
-    where: {
-      id,
-    },
+import { actionClient } from "@/app/_lib/safe-action";
+
+export const deleteProduct = actionClient
+  .schema(deleteProductSchema)
+  .action(async ({ parsedInput: { id } }) => {
+    await db.product.delete({
+      where: {
+        id,
+      },
+    });
+    revalidatePath("/products");
   });
-  revalidatePath("/products");
-};
--- app/products/_components/delete-dialog-content.tsx
+++ app/products/_components/delete-dialog-content.tsx
@@ -8,6 +8,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/app/_components/ui/alert-dialog";
+import { useAction } from "next-safe-action/hooks";
 import { toast } from "sonner";
 
 interface DeleteProductDialogContentProps {
@@ -17,15 +18,16 @@ interface DeleteProductDialogContentProps {
 const DeleteProductDialogContent = ({
   productId,
 }: DeleteProductDialogContentProps) => {
-  const handleContinueClick = async () => {
-    try {
-      await deleteProduct({ id: productId });
+  const { execute: executeDeleteProduct } = useAction(deleteProduct, {
+    onSuccess: () => {
       toast.success("Produto deletado com sucesso!");
-    } catch (error) {
-      console.error(error);
+    },
+    onError: () => {
       toast.error("Erro ao deletar o produto. Tente novamente.");
-    }
-  };
+    },
+  });
+  const handleContinueClick = () => executeDeleteProduct({ id: productId });
+
   return (
     <AlertDialogContent>
       <AlertDialogHeader>
